19febc105cb3c5d5a579ab307765506d7468e203
Guillermo Salas <98062656+BarbellG33k@users.noreply.github.com>
2025-06-19 18:35:23 -0500

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Search, MapTrifold, User } from 'lucide-react-native';
+import { Chrome as Home, Search, Map as MapTrifold, User } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
